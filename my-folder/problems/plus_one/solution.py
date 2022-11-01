252473da1b2220711b72b05fc84cf393b5eb39af
Jack S <96142280+jackschuld@users.noreply.github.com>
2022-11-01 20:51:22 +0000

    Sync LeetCode submission - Plus One (python3)

--- my-folder/problems/plus_one/solution.py
+++ my-folder/problems/plus_one/solution.py
@@ -1,19 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # If list length is only 1
-        if len(digits) <= 1:
-            if 9 in digits:
-                return [1, 0]
-            else:
-                digits = [x+1 for x in digits]
-        
-        # If last number is not 9, then add 1 and return
-        elif digits[-1] != 9:
+        if digits[-1] != 9:
             digits[-1] += 1
-        
-        # If last number is 9, change to zero and check rest of list
-        else:
+        elif len(digits) > 1:
             digits = self.plusOne(digits[:-1])
             digits.append(0)
-            
+        else:
+            digits = [1, 0]
         return digits
