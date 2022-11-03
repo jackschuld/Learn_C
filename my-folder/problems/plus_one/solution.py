c039a75b906cf5afd22fbe5a68922abe3e240a27
Jack S <96142280+jackschuld@users.noreply.github.com>
2022-11-03 20:35:39 +0000

    Sync LeetCode submission - Plus One (python3)

--- my-folder/problems/plus_one/solution.py
+++ my-folder/problems/plus_one/solution.py
@@ -1,10 +1,66 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if digits[-1] != 9:
-            digits[-1] += 1
-        elif len(digits) > 1:
+        if len(digits) == 1:
+            if digits[0] == 9:
+                digits = [1, 0]
+            else:
+                digits = [digit+1 for digit in digits]
+            return digits
+        
+        tail = digits[-1]
+        if tail == 9:
+            tail = 0
             digits = self.plusOne(digits[:-1])
-            digits.append(0)
         else:
-            digits = [1, 0]
+            tail += 1
+            digits = digits[:-1]
+        digits.append(tail)
         return digits
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #if digits[-1] != 9:
+        #    digits[-1] += 1
+        #elif len(digits) > 1:
+        #    digits = self.plusOne(digits[:-1])
+        #    digits.append(0)
+        #else:
+        #    digits = [1, 0]
+        #return digits
