880ae8c222f9c19dc62516f3b61efa1af48d5e16
Jack S <96142280+jackschuld@users.noreply.github.com>
2022-11-03 18:53:30 +0000

    Sync LeetCode submission - Longest Common Prefix (python3)

--- my-folder/problems/longest_common_prefix/solution.py
+++ my-folder/problems/longest_common_prefix/solution.py
@@ -1,13 +1,29 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        current = strs[0]
+        output = strs[0]
         for s in strs:
-            if len(s) < len(current):
-                current = current[:len(s)]
-            if current != s:
-                for i in range(len(current)):
-                    if s[i] != current[i]:
-                        current = s[:i]
+            if len(s) < len(output):
+                output = output[:len(s)]
+            if output != s:
+                for i in range(len(output)):
+                    if s[i] != output[i]:
+                        output = s[:i]
                         break
-        return current
+        return output
+            
+    
+        
+        
+        
+        
+        #current = strs[0]
+        #for s in strs:
+         #   if len(s) < len(current):
+          #      current = current[:len(s)]
+           # if current != s:
+            #    for i in range(len(current)):
+             #       if s[i] != current[i]:
+              #          current = s[:i]
+               #         break
+        #return current
         
