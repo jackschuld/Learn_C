31fe74c606fe9a9ea2bb4816a95e381c78085c51
Jack S <96142280+jackschuld@users.noreply.github.com>
2022-11-04 18:18:07 +0000

    Sync LeetCode submission - Sqrt(x) (python3)

--- my-folder/problems/sqrt(x)/solution.py
+++ my-folder/problems/sqrt(x)/solution.py
@@ -1,11 +1,20 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l, r = 0, x
-        while l <= r:
-            mid = l + (r-l)//2
-            if mid * mid <= x < (mid+1)*(mid+1):
-                return mid
-            elif x < mid * mid:
-                r = mid - 1
+        sq = 1
+        while sq <= x:
+            if sq*sq > x:
+                return (sq-1)
             else:
-                l = mid + 1
+                sq+=1
+        return x
+        
+
+        #l, r = 0, x
+        #while l <= r:
+        #    mid = l + (r-l)//2
+        #    if mid * mid <= x < (mid+1)*(mid+1):
+        #        return mid
+        #    elif x < mid * mid:
+        #        r = mid - 1
+        #    else:
+        #        l = mid + 1
