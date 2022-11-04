9ac218bbdb3a215c8ab490b98b41be746a347e7c
Jack S <96142280+jackschuld@users.noreply.github.com>
2022-11-04 21:08:51 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- my-folder/problems/climbing_stairs/solution.py
+++ my-folder/problems/climbing_stairs/solution.py
@@ -1,8 +1,25 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         prev = 0
-        current = 1
+        curr = 1
         for i in range(n):
-            current += prev
-            prev = current - prev
-        return current
+            curr += prev
+            prev = curr - prev
+        return curr
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #prev = 0
+        #current = 1
+        #for i in range(n):
+        #    current += prev
+        #    prev = current - prev
+        #return current
