232ca6a6377cb9d427cf094153b2f3714e7b7f82
Jack S <96142280+jackschuld@users.noreply.github.com>
2022-11-07 15:56:26 +0000

    Sync LeetCode submission - Same Tree (python3)

--- my-folder/problems/same_tree/solution.py
+++ my-folder/problems/same_tree/solution.py
@@ -10,9 +10,25 @@ class Solution:
             return True
         elif p is None or q is None:
             return False
-        
         if p.val != q.val:
             return False
-        
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            
+            
+            
+            
+            
+            
+            
+    
+    
+    #if p is None and q is None:
+        #    return True
+        #elif p is None or q is None:
+        #    return False
+       # 
+       # if p.val != q.val:
+       #     return False
+       # 
+       # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
