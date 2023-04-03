4deeb8eb6f20e890f602d9c5a9f42cfde38c4a83
Jack S <96142280+jackschuld@users.noreply.github.com>
2023-04-03 17:40:46 +0000

    Sync LeetCode submission - Lemonade Change (python3)

---
+++ my-folder/problems/lemonade_change/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        cashRegister = []
+        for bill in bills:
+            if bill == 20:
+                if 5 in cashRegister and 10 in cashRegister:
+                    cashRegister.pop(cashRegister.index(5))
+                    cashRegister.pop(cashRegister.index(10))
+                elif cashRegister.count(5) > 2:
+                    for pop in range(3):
+                        cashRegister.pop(cashRegister.index(5))
+                else:
+                    return False
+            elif bill == 10:
+                if not 5 in cashRegister:
+                    return False
+                cashRegister.pop(cashRegister.index(5))
+            cashRegister.append(bill)
+        return True
+
+        
