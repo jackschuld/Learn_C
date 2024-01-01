97924ac8ec9011e40d0aa00cdb1bf6b2bb60a66d
Jack S <96142280+jackschuld@users.noreply.github.com>
2024-01-01 23:01:48 +0000

    Sync LeetCode submission - Two Sum (c)

--- my-folder/problems/two_sum/solution.c
+++ my-folder/problems/two_sum/solution.c
@@ -1,21 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     for (int i = 0; i < numsSize; i++) {
-        for (int j = 0; j < numsSize; j++) {
-            if (i == j) {
-                continue; 
-            }
+        for (int j = 1+i; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
-                int* result = (int*)malloc(2 * sizeof(int));
-                if (result == NULL) {
-                    return NULL;
-                }
-
+                int* result = malloc(2 * sizeof(int));
                 result[0] = i;
                 result[1] = j;
                 *returnSize = 2;
@@ -24,5 +15,5 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
             }
         }
     }
-    return NULL;
+    return -1;
 }
